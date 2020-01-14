8793a6968f55af7e5a7a0dbc75b44ed1ef0f60fd
AydanKusland <55348746+AydanKusland@users.noreply.github.com>
2020-01-14 10:04:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 const decompose = n =>  { //625
   let answer = [n-1] //624
   let ostatok = 2*n-1 // 1249
-  //const total = n*n // 390625
+  let floorKorenOstatka, counter = 0
   while (ostatok !== 0) {//1-1249/2-24/3-8/
     floorKorenOstatka = Math.floor(Math.sqrt(ostatok))
     answer.unshift(floorKorenOstatka) //1-35/2-4/3-2/4-2/
     ostatok = ostatok - floorKorenOstatka * floorKorenOstatka //1-24/2-8/3-4/4-0
-    if (answer[0] === answer[1]) {
+    counter++
+    if (counter === 20 || answer.includes(0)) {
+      answer = answer.slice(answer.length - 1)
+      answer[0] = answer[0] - 1
+      ostatok = n*n - answer[0] * answer[0]
+    }
+    if (answer[0] === answer[1] || ostatok === 2 || ostatok === 3 ) {
       answer = answer.slice(answer.length - 2)
       answer[0] = answer[0] - 1
-      ostatok = 2*n-1 - answer[0] * answer[0]}
+      ostatok = 2*n-1 - answer[0] * answer[0]
+    }
 
     console.log(ostatok);
     console.log(answer);
   }
-  return console.log(answer);
-  
-}     
-  //decompose(7100)
-decompose(625)
+  return console.log(answer.sort((a,b) => a < b));
+}    
+decompose(12) 
 
 
 const duplicateEncode = word => word
