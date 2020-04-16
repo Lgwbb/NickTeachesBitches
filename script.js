d667d493cfe72e79843688061b1ea8681b670205
AydanKusland <55348746+AydanKusland@users.noreply.github.com>
2020-04-16 18:29:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,36 @@
-const decompose = n =>  { //625
-  let answer = [n-1] //624
-  let ostatok = 2*n-1 // 1249
-  let floorKorenOstatka, counter = 0
-  while (ostatok !== 0) {//1-1249/2-24/3-8/
-    floorKorenOstatka = Math.floor(Math.sqrt(ostatok))
-    answer.unshift(floorKorenOstatka) //1-35/2-4/3-2/4-2/
-    ostatok = ostatok - floorKorenOstatka * floorKorenOstatka //1-24/2-8/3-4/4-0
-    counter++
-    if (counter === 20 || answer.includes(0)) {
-      answer = answer.slice(answer.length - 1)
-      answer[0] = answer[0] - 1
-      ostatok = n*n - answer[0] * answer[0]
+function simplify(poly){
+    let polyObject = {
+        initial_poly: poly,
+        members_positons: [0],
+        members_array: [],
+        poly_digits: [],
+
+        //sorted_poly: [],
+        get_members_posotions: function() {
+            for (let i = 2; i < this.initial_poly.length; i++) {
+                if (this.initial_poly.charAt(i) == '-' || this.initial_poly.charAt(i) == '+') this.members_positons.push(i)}},
+        get_members_array: function() {
+            for (let i = 0; i < this.members_positons.length; ++i) this.members_array.push(this.initial_poly.slice(this.members_positons[i],this.members_positons[i+1]))},
+        //sort_poly: function ()
+        get_poly_digits: function() {this.members_array.forEach(v => {v.length - v.search(/\d/)})}
+            
+
+
+
+
+
+
     }
-    if (answer[0] === answer[1] || ostatok === 2 || ostatok === 3 ) {
-      answer = answer.slice(answer.length - 2)
-      answer[0] = answer[0] - 1
-      ostatok = 2*n-1 - answer[0] * answer[0]
-    }
+    polyObject.get_members_posotions()
+    polyObject.get_members_array()
+    polyObject.get_poly_digits()
 
-    console.log(ostatok);
-    console.log(answer);
-  }
-  return console.log(answer.sort((a,b) => a < b));
-}    
-decompose(12) 
+console.log(polyObject);
+
+}
 
 
-const duplicateEncode = word => word
-.toLowerCase()
-.split('')
-.map( (curVal) => (word.indexOf(curVal) !== (word.lastIndexOf(curVal))) ? ')' : '(' )
-.join('')
+  simplify('-a+5ab+3a-c-2a')
 
-
-
-const rot13 = message =>
-  const ascii = []
-  message.toLowerCase()
-  .split('')
-  .map( (curVal, index, arr) => {(charCodeAt(index) > 96 && charCodeAt(index) < 123) ?  : curval })
-  .join('')
+   console.log('-3b'.search(/\d+/));
+   console.log(/\d/.exec('-33b'));
